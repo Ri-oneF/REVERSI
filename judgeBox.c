eb057c93aec473c5d8105d69a1a7196bed98fc7e
osakana <is0342fv@ed.ritsumei.ac.jp>
2016-05-24 13:40:53 +0900

    judge_box.c追加 動作確認不足かもしれません aとbの値については信用してしまっているので、範囲外の値はmain関数内で弾いてください。

--- judgeBox.c
+++ judgeBox.c
@@ -1,11 +1,90 @@
 #include "REVERSI.h"
 
+/*
+  dirc:
+	0:下
+	1:右下
+	2:右
+	3:右上
+	4:上
+	5:左上
+	6:左
+	7:左下
+*/
+
+int judge_line(int dirc, int a, int b);
+
 int judgeBox(int a, int b)
 {
-	// 引数として受け取った座標に、playerはコマを置くことができるか
 
-	// 置けたら 1
-	// 置けなかったら 0
+	if(board[a][b] != MARK){
+		printf("\n\n\aその場所には既に石が置かれています\n\n");
+		return 0;
+	}
+	/* とりあえずその場が何も置かれてない状態であることを確認 */
+
+
+	int dirc;
+
+	for(dirc=0;dirc<8;dirc++){ /* 8方向について一つづつ調べるというループ */
+		if(judge_line(dirc,a,b) == 1) return 1;
+		/* 一度でも置けると判断すれば1を返す */
+	}
 
 	return 0;
+	/* 残念ながら置けませんでした */
+
 }
+
+int judge_line(int dirc,int a,int b)
+{
+	int yoko, tate;
+	/* 縦、横のループカウンタ */
+	int yc, tc;
+	/* 方向を表す用の加算・減算用変数 */
+	int flag=0;
+	/* 最低でもひとつは相手の色と隣り合ってたよ！っていうフラグ */
+
+	int enemy = (player) ? WHITE : BLACK;
+	/* 相手の色をenemy変数に記憶 */
+
+
+	/* 方向によってカウンタを動かす向きを変える */
+	switch(dirc){
+		case 0: yc= 0; tc= 1; break;
+		case 1: yc= 1; tc= 1; break;
+		case 2: yc= 1; tc= 0; break;
+		case 3: yc= 1; tc=-1; break;
+		case 4: yc= 0; tc=-1; break;
+		case 5: yc=-1; tc=-1; break;
+		case 6: yc=-1; tc= 0; break;
+		case 7: yc=-1; tc= 1; break;
+		default :puts("error in judge_line");
+	}
+
+	/* 相手の色が隣り合う限りずらして行く */
+	for (yoko=a+yc,tate=b+tc;;yoko+=yc,tate+=tc)
+	{
+		if(board[yoko][tate] == enemy){
+			flag=1; /* 最低でも一個は相手の色と隣り合うことをここで記憶しておく */
+			continue;
+		} else {
+			break;
+		}
+	}
+
+	/* 自分の色ではさめてるかな？ */
+	if(board[yoko][tate] == player && flag==1){
+		return 1; /* 無事に挟まれていました */
+	} else {
+		return 0; /* 挟まれてないやん */
+	}
+
+}
+
+
+
+
+
+
+
